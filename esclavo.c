45110323ec699d40973fab2f1699fc41943374ad
Lautaro Gazzaneo <67807043+lgazzaneo@users.noreply.github.com>
2022-09-10 17:45:09 -0300

    adding goals

--- esclavo.c
+++ esclavo.c
@@ -1,2 +1,6 @@
 // This is a personal academic project. Dear PVS-Studio, please check it.
 // PVS-Studio Static Code Analyzer for C, C++, C#, and Java: http://www.viva64.com
+//MUST to recieve the paths of the files to process y it MUST initiate the program corresponding for processing them (md5sum)
+//MUST send the information relevant to processing to the process aplication
+//MUSTN'T dump the result in a file in disc, to then read it from the slave, it MUST recieve the output of md5sum utilizing
+//some mechanism of IPC more sophisticated
